dcd8b02b1d467dc1ed14ea52d9d7efb7bf4b59e2
frabbit <frabbit77@googlemail.com>
2012-02-06 09:07:17 +0100

    added notImplemented error changed getOrElseNoMacro to getOrElse

--- src/scuts/Scuts.hx
+++ src/scuts/Scuts.hx
@@ -1,6 +1,8 @@
 package scuts;
 
-#if macro
+import haxe.PosInfos;
+
+#if (macro || display)
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
@@ -13,20 +15,35 @@ class Scuts
   public static function id <T> (a:T):T return a
   
   public static function abstractMethod <T>():T {
-    throw "This method is abstract, you must override it";
-    return null;
+    return error("This method is abstract, you must override it");
   }
   
-  public static function error <T>(msg:String):T 
+  public static function notImplemented <T>():T {
+    return error("This method is not yet implemented");
+  }
+  
+  public static function checkNotNull <T>(v:T):T {
+    #if debug
+    Assert.assertNotNull(v);
+    #end
+    return error("This method is not yet implemented");
+  }
+  
+  public static function error <T>(msg:String, ?posInfos:PosInfos):T 
   {
+    #if macro
+    return macroError(msg, Context.currentPos(), posInfos);
+    #else
     throw msg;
     return null;
+    #end
   }
   
+  
   #if macro
-  public static function macroError <T>(msg:String, ?p:Position):T 
+  public static function macroError <T>(msg:String, ?p:Position, ?posInfos:PosInfos):T 
   {
-    var p1 = p.getOrElseNoMacro(Context.currentPos());
+    var p1 = p.getOrElse(Context.currentPos());
     throw new Error(msg,p1);
     return null;
   }
