85e161c8510226830091f9b96156fc67fbe8d371
frabbit <frabbit77@googlemail.com>
2012-02-06 09:06:14 +0100

    asserts should return types (composable)

--- src/scuts/Assert.hx
+++ src/scuts/Assert.hx
@@ -5,69 +5,75 @@ import Type;
 
 class Assert 
 {
-  private static inline function doAssert (expr : Bool, assertId : String, ?message : String, ?posInfos:PosInfos) 
+  private static inline function doAssert (expr : Bool, assertId : String, ?message : String, ?posInfos:PosInfos):Void
   {
-	if (!(expr)) {
-		var msg = assertId + " failed: " + message + " at " + Std.string(posInfos);
-		trace(msg);
-		throw msg;
-	}
+    if (!(expr)) {
+      if (message == null) message = "(no message)";
+      var posStr = posInfos.fileName + " at " + posInfos.lineNumber + " (" + posInfos.className + "." + posInfos.methodName + ")"; 
+      var msg = posStr + ": " + assertId + " failed: " + message;
+      Scuts.error(msg, posInfos);
+    } 
   }
   
   /**
    * throws an assertion error in debug mode if expr is not true
    */
-	public static inline function assertTrue (expr:Bool, ?message:String, ?posInfos:PosInfos):Void 
+	public static inline function assertTrue (expr:Bool, ?message:String, ?posInfos:PosInfos):Bool 
 	{
 		#if debug
 		doAssert(expr, "assertTrue", message, posInfos);
 		#end
+    return expr;
 	}
   
   /**
    * throws an assertion error in debug mode if expr is not true
    */
-	public static inline function assertIsObject (val:Dynamic, ?message:String, ?posInfos:PosInfos):Void 
+	public static inline function assertIsObject <T>(val:T, ?message:String, ?posInfos:PosInfos):T 
 	{
 		#if debug
 		doAssert(Reflect.isObject(val), "assertIsObject", message, posInfos);
 		#end
+    return val;
 	}
   /**
    * throws an assertion error in debug mode.
    */
-  public static inline function fail (?message:String, ?posInfos:PosInfos):Void 
+  public static inline function fail <T>(?message:String, ?posInfos:PosInfos):T 
 	{
 		#if debug
 		doAssert(false, "fail", message, posInfos);
 		#end
+    return Scuts.error("fail");
 	}
 	
 	/**
    * throws an assertion error in debug mode if expected doesn't equals actual.
    */
-  public static inline function assertEquals <T>(expected:T, actual:T, ?message:String, ?posInfos:PosInfos):Void 
+  public static inline function assertEquals <T>(expected:T, actual:T, ?message:String, ?posInfos:PosInfos):T 
 	{
 		#if debug
         message = message == null ? "expected " + expected + " but was " + actual : message;
 		doAssert(expected == actual, "assertEquals", message, posInfos);
 		#end
+    return actual;
 	}
   
   /**
    * throws an assertion error in debug mode if a is null.
    */
-  public static inline function assertNotNull (a:Dynamic, ?message:String = null, ?posInfos:PosInfos):Void 
+  public static inline function assertNotNull <T>(a:T, ?message:String = null, ?posInfos:PosInfos):T 
 	{
 		#if debug
 		doAssert(a != null, "assertNotNull", message, posInfos);
 		#end
+    return a;
 	}
   
 	/**
    * throws an assertion error in debug mode if all elements of a are null.
    */
-	public static inline function assertAllNotNull (a:Iterable<Dynamic>, ?message:String = null, ?posInfos:PosInfos):Void 
+	public static inline function assertAllNotNull <T>(a:Iterable<T>, ?message:String = null, ?posInfos:PosInfos):Iterable<T> 
 	{
 		
 		#if debug
@@ -78,22 +84,24 @@ class Assert
 			i++;
 		}
 		#end
+    return a;
 	}
 	
   /**
    * throws an assertion error in debug mode if a is not null.
    */
-	public static function assertNull <T>(a:T, ?message:String, ?posInfos:PosInfos):Void 
+	public static function assertNull <T>(a:T, ?message:String, ?posInfos:PosInfos):T 
 	{
 		#if debug
 		doAssert(a == null, "assertNotNull", message, posInfos);
 		#end
+    return a;
 	}
   
   /**
    * throws an assertion error in debug mode if value is not between min and max.
    */
-  public static inline function assertFloatInRange (value : Float, min : Float, max : Float, ?message : String, ?posInfos:PosInfos) 
+  public static inline function assertFloatInRange (value : Float, min : Float, max : Float, ?message : String, ?posInfos:PosInfos):Float 
 	{
 		#if debug
 		message = message == null 
@@ -101,12 +109,14 @@ class Assert
 				: message;
         doAssert( min <= value && value <= max, "assertInRange", message, posInfos);
 		#end
-    }
+    return value;
+  }
+    
 	
   /**
    * throws an assertion error in debug mode if value is not between min and max.
    */
-	public static inline function assertIntInRange (value : Int, min : Int, max : Int, ?message : String, ?posInfos:PosInfos) 
+	public static inline function assertIntInRange (value : Int, min : Int, max : Int, ?message : String, ?posInfos:PosInfos):Int
 	{
 		#if debug
 		message = message == null 
@@ -114,32 +124,35 @@ class Assert
 				: message;
         doAssert( min <= value && value <= max, "assertInRange", message, posInfos);
 		#end
-    }
+    return value;
+  }
 	
   /**
    * throws an assertion error in debug mode if expr is true.
    */
-  public static inline function assertFalse (expr:Bool, ?message:String, ?posInfos:PosInfos):Void 
+  public static inline function assertFalse (expr:Bool, ?message:String, ?posInfos:PosInfos):Bool 
 	{
 		#if debug
 		doAssert(!expr, "assertFalse", message, posInfos);
 		#end
+    return expr;
 	}
   
   /**
    * throws an assertion error in debug mode if obj is not of type objType.
    */
-	public static inline function assertType (obj:Dynamic, objType:Dynamic, ?posInfos:PosInfos) 
+	public static inline function assertType <T>(obj:T, objType:Dynamic, ?posInfos:PosInfos):T 
 	{
 		#if debug
 		doAssert(Std.is(obj, objType), "assertType", "expected type " + objType + ", but was " + Type.getClass(obj), posInfos);
 		#end
+    return obj;
 	}
 	
   /**
    * throws an assertion error in debug mode if obj is a class instance.
    */
-	public static inline function assertIsClassInstance (obj:Dynamic, ?posInfos:PosInfos) 
+	public static inline function assertIsClassInstance <T>(obj:T, ?posInfos:PosInfos):T 
 	{
 		#if debug
 		var isClassInstance = switch (Type.typeof(obj)) 
@@ -149,6 +162,7 @@ class Assert
 		}
 		doAssert(isClassInstance, "assertType", "expected type " + obj + ", but was " + Type.getClass(obj), posInfos);
 		#end
+    return obj;
 	}
 	
 	
