b2d22f081afbdbfc329dcb7bfb5a18377f067235
frabbit <frabbit77@googlemail.com>
2012-02-21 20:00:01 +0100

    - fixed class name - fixed setField

--- src/scuts/core/extensions/ObjectExt.hx
+++ src/scuts/core/extensions/ObjectExt.hx
@@ -1,30 +1,19 @@
 package scuts.core.extensions;
 
 
-class Objects 
+class ObjectExt
 {
-
-  public static inline function field (v:{}, field:String):Dynamic {
-    return Reflect.field(v, field);
-  }
+  public static inline function field (v:{}, field:String):Dynamic return Reflect.field(v, field);
   
-  public static inline function setField < T /* : {} */ > (v:T, field:String, value:Dynamic):T 
+  
+  public static inline function setField  (v:{}, field:String, value:Dynamic):Dynamic
   {
     Reflect.setField(v, field, value);
     return v;
   }
   
-  public static inline function hasField (v:{}, field:String):Bool 
-  {
-    return Reflect.hasField(v, field);
-  }
-  
-  public static inline function fields (v:{}, field:String):Array<String> {
-    return Reflect.fields(v);
-  }
-  
-  
-  
-  
+  public static inline function hasField (v:{}, field:String):Bool return Reflect.hasField(v, field);
+
+  public static inline function fields (v:{}, field:String):Array<String> return Reflect.fields(v);
   
 }
