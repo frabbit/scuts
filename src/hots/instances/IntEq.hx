7f1a944609eaead139abc0d7fceccccf7ab5d0d1
frabbit <frabbit77@googlemail.com>
2012-02-21 20:11:24 +0100

    moved eq implementation to IntExt

--- src/hots/instances/IntEq.hx
+++ src/hots/instances/IntEq.hx
@@ -2,14 +2,13 @@ package hots.instances;
 
 
 import hots.classes.EqAbstract;
+import scuts.core.extensions.IntExt;
 
 class IntEqImpl extends EqAbstract<Int> {
   
   public function new () {}
   
-  override public inline function eq (a:Int, b:Int):Bool {
-    return a == b;
-  }
+  override public inline function eq (a:Int, b:Int):Bool return IntExt.eq(a,b)
   
 }
 
