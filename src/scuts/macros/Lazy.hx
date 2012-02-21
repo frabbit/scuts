3c78fa2370c669f5c72ff6880fc99f1a120cefef
frabbit <frabbit77@googlemail.com>
2012-02-21 20:21:32 +0100

    implementation moved to scutsCore

--- src/scuts/macros/Lazy.hx
+++ src/scuts/macros/Lazy.hx
@@ -1,19 +1,3 @@
 package scuts.macros;
 
-#if macro
-import haxe.macro.Expr.ExprRequire;
-import haxe.macro.Expr;
-import scuts.mcore.Parse;
-#end
-class Lazy 
-{
-
-  @:macro public static function expr(expr:ExprRequire<Dynamic>):Expr 
-  {
-    var op = "scuts.core.types.Option";
-    var ops = "scuts.core.extensions.OptionExt";
-    
-    return Parse.parse("{var r = null; var isSet = false; function () { if (!isSet) {r = $0; isSet = true;} return r; }}", [expr], expr.pos);
-  }
-  
-}
+typedef Lazy = scuts.core.macros.Lazy;
