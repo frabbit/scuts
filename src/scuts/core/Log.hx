b49a6fc5642cb34680bee6b84a72ee757639cb1e
frabbit <frabbit77@googlemail.com>
2012-03-07 14:33:54 +0100

    - whitespace fix

--- src/scuts/core/Log.hx
+++ src/scuts/core/Log.hx
@@ -3,18 +3,13 @@ import haxe.PosInfos;
 
 class Log 
 {
-
-  
-	public static  function traced <T>(v:T, ?show:T->String, ?pos:PosInfos):T {
+	public static function traced <T>(v:T, ?show:T->String, ?pos:PosInfos):T 
+  {
     #if debug
     var t = if (show != null) show(v) else Std.string(v);
     haxe.Log.trace(t, pos);
     #end
     return v;
   }
-	
-  /*
-	
-	*/
 	
 }
