61f66b982ed3ad1edb7abda4aaee7d95b6776b0e
frabbit <frabbit77@googlemail.com>
2012-03-07 14:35:28 +0100

    Added Tool ImportAllBuilder which creates a class that imports all modules of a given package

---
+++ src/scuts/core/tools/ImportAllBuilder.hx
@@ -0,0 +1,51 @@
+package scuts.core.tools;
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+import neko.FileSystem;
+import neko.io.File;
+using scuts.core.extensions.StringExt;
+
+class ImportAllBuilder
+{
+
+  public static function build(pack:String, target:String) 
+  {
+    var classPaths = Context.getClassPath();
+    function create1 (pack:String, buf:StringBuf) 
+    {
+      var parts = pack.split(".");
+      var packPath = "/" + pack.split(".").join("/") + (pack.length > 0 ? "/" : "");
+      var packBase = pack + (pack.length > 0 ? "." : "");
+      
+      for (cp in classPaths) 
+      {
+        var folder = cp + packPath;
+        for (f in FileSystem.readDirectory(folder)) 
+        {
+          var fullPath = folder + "/" + f;
+          if (FileSystem.isDirectory(fullPath)) 
+          {
+            create1(packBase + f, classContent);
+          } 
+          else if (fullPath.endsWith(".hx")) 
+          {
+            var lastSlash = fullPath.lastIndexOf("/")+1;
+            buf.add("import ");
+            buf.add(packBase);
+            buf.add(fullPath.substr(lastSlash, fullPath.length - 3 - lastSlash));
+            buf.add(";\n");
+          }
+          
+        }
+      }
+    }
+    var buf = new StringBuf();
+    create1(pack, buf);
+    buf.add("class ");
+    buf.add(target);
+    buf.add(" { static function main () {} }");
+    var output = File.write(target + ".hx", false);
+    output.writeString(buf.toString());
+  }
+  
+}
