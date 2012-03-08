7ede9d568205028ac945a5b6dccf0e22298b3380
frabbit <frabbit77@googlemail.com>
2012-03-08 12:55:44 +0100

    - added hxml files - added Testbase

---
+++ test/AllTests.hx
@@ -0,0 +1,44 @@
+package;
+
+//import scuts.mcore.CheckTest;
+
+//import scuts.mcore.TypeTest;
+
+#if macro
+import neko.Lib;
+import neko.Sys;
+
+
+
+#else 
+import scuts.core.extensions.StringExtTest;
+#end
+
+
+
+import utest.Runner;
+import utest.ui.Report;
+
+/**
+ * ...
+ * @author $(DefaultUser)
+ */
+
+class AllTests 
+{
+  public static function main() 
+    {
+      var runner = new Runner();
+      #if macro
+      
+      #else
+      runner.addCase(new StringExtTest());
+      #end
+      
+      Report.create(runner);
+      #if macro
+      Lib.println("---------------------------------------------------------------------------------------------");
+      #end
+      runner.run();
+    }
+}
---
+++ test/scuts/core/extensions/StringExtTest.hx
@@ -0,0 +1,20 @@
+package scuts.core.extensions;
+
+import scuts.core.extensions.StringExt;
+import utest.Assert;
+
+private typedef S = StringExt;
+
+class StringExtTest 
+{
+
+  public function new () {}
+  
+  public function testTimes_should_return_the_string_5_times() 
+  {
+    var s = "a";
+    
+    Assert.equals("aaaaa",S.times(s, 5));
+  }
+  
+}
