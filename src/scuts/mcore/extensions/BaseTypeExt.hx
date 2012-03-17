e8f50ae075967e273a8e17f29768901345c47293
frabbit <frabbit77@googlemail.com>
2012-03-17 05:24:05 +0100

    added wildcards support for BaseType -> ComplexType conversions

--- src/scuts/mcore/extensions/BaseTypeExt.hx
+++ src/scuts/mcore/extensions/BaseTypeExt.hx
@@ -4,22 +4,23 @@ import haxe.macro.Type;
 import haxe.macro.Expr;
 
 using scuts.core.extensions.ArrayExt;
-
+using scuts.core.extensions.DynamicExt;
 class BaseTypeExt 
 {
 
-  public static function toTypePath(b:BaseType, params:Array<Type>):TypePath 
+  public static function toTypePath(b:BaseType, params:Array<Type>, wildcards:Array<Type>):TypePath 
   {
     return {
       pack: b.pack,
       name: b.name,
-      params: params.map(function (p) return TPType(TypeExt.toComplexType(p)))
+      params: params.map(function (p) return TPType(TypeExt.toComplexType(p, wildcards)))
     }
   }
   
-  public static function toComplexType (b:BaseType, params:Array<Type>):ComplexType
+  public static function toComplexType (b:BaseType, params:Array<Type>, wildcards:Array<Type>):ComplexType
   {
-    return ComplexType.TPath(toTypePath(b, params));
+    wildcards.nullGetOrElse(function () return []);
+    return ComplexType.TPath(toTypePath(b, params, wildcards));
   }
   
 }
