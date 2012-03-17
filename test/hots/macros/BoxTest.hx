8ee83006998151141933161bde5785f89c6e7de7
frabbit <frabbit77@googlemail.com>
2012-03-17 15:45:09 +0100

    improved BoxTest and UtilsTest

--- test/hots/macros/BoxTest.hx
+++ test/hots/macros/BoxTest.hx
@@ -3,25 +3,16 @@ import hots.In;
 import hots.Of;
 import utest.Assert;
 
-/**
- * ...
- * @author 
- */
+// If this test compiles, it works
 
 class BoxTest 
 {
 
   public function new() {}
   
-  public function testBox () {
-    var type1 = [1];
-    
-    var t1:Of<Array<In>, Int> = Box.box(type1);
-
-    Assert.isTrue(true);
-  }
   
-  public function testUnbox () {
+  public function testUnbox_Simple () 
+  {
     var type1:Of<Array<In>, Int> = null;
     
     var t1:Array<Int> = Box.unbox(type1);
@@ -29,15 +20,26 @@ class BoxTest
     Assert.isTrue(true);
   }
   
-  public function testUnbox2 () {
+  public function testUnbox_InnerType () 
+  {
     var type1:Of<Of<Array<In>, Array<In>>, Int> = null;
     
-    var t1:Of<Array<In>, Array<Int>> = type(Box.unbox(type1));
+    var t1:Of<Array<In>, Array<Int>> = Box.unbox(type1);
 
     Assert.isTrue(true);
   }
   
-  public function testBox_2Times () {
+  public function testBox_1time () 
+  {
+    var type1 = [1];
+    
+    var t1:Of<Array<In>, Int> = Box.box(type1);
+
+    Assert.isTrue(true);
+  }
+  
+  public function testBox_2Times () 
+  {
     var type2 = [[1]];
     
     var t2:Of<Array<In>, Array<Int>> = Box.box(type2);
@@ -46,25 +48,14 @@ class BoxTest
     Assert.isTrue(true);
   }
   
-  public function testBox_3Times () {
+  public function testBox_3Times () 
+  {
     var t = [[[1]]];
     
     var t1:Of<Array<In>, Array<Array<Int>>> = Box.box(t);
     var t2:Of<Of<Array<In>, Array<In>>, Array<Int>> = Box.box(t1);
     var t3:Of<Of<Of<Array<In>, Array<In>>, Array<In>>, Int> = Box.box(t2);
-    
-    //Box.box(4);
-    
-    Assert.isTrue(true);
-  }
-  
-  public function testBox_100Times () {
-    var t = [[[1]]];
-    
-    for (i in 0...10000) {
-      var t1:Of<Array<In>, Array<Array<Int>>> = Box.box(t);
-    }
-    
+
     Assert.isTrue(true);
   }
   
--- test/hots/macros/utils/UtilsTest.hx
+++ test/hots/macros/utils/UtilsTest.hx
@@ -1,24 +1,213 @@
 package hots.macros.utils;
-#if macro
+#if (macro || display)
 import haxe.macro.Context;
+import scuts.core.extensions.ArrayExt;
+import scuts.core.extensions.OptionExt;
+import scuts.core.extensions.StringExt;
+import scuts.core.extensions.Tup2Ext;
+import scuts.core.types.Tup2;
+import scuts.mcore.MContext;
 import scuts.mcore.extensions.TypeExt;
 import scuts.mcore.Parse;
 import scuts.mcore.Print;
+import scuts.Scuts;
 import utest.Assert;
-
+import scuts.core.types.Option;
 using scuts.core.extensions.OptionExt;
+import hots.macros.utils.Utils;
+using scuts.core.extensions.DynamicExt;
+using scuts.core.extensions.Function0Ext;
 #end
 #if !macro
 class XXX {}
 class YYY {}
+interface D {}
+interface E {}
+interface C implements D, implements E {}
+class S implements C {}
+class U extends S{}
+
+interface V<T> {}
+
+class Z implements V<Int> {}
+
+
+class Q<K, TT> implements V<TT> {}
+
+class Z2<K> extends Q<Int, K> {}
+
 #end
 
-#if macro
+#if (macro || display)
 class UtilsTest 
 {
   static var FQ_XXX = "hots.macros.utils.UtilsTest.XXX";
   static var FQ_YYY = "hots.macros.utils.UtilsTest.YYY";
+  static var FQ_U = "hots.macros.utils.UtilsTest.U";
+  static var FQ_S = "hots.macros.utils.UtilsTest.S";
+  static var FQ_C = "hots.macros.utils.UtilsTest.C";
+  static var FQ_D = "hots.macros.utils.UtilsTest.D";
+  static var FQ_Z = "hots.macros.utils.UtilsTest.Z";
+  static var FQ_Z2 = "hots.macros.utils.UtilsTest.Z2";
+  static var FQ_V = "hots.macros.utils.UtilsTest.V";
   public function new() {}
+  
+  
+  public function testRemap () 
+  {
+    var t = MContext.getType(FQ_V).getOrError("Cannot get type of " + FQ_V);
+    
+    var ct = TypeExt.asClassType(t).getOrError("Cannot convert to Class Type");
+    
+    var paramsCur = ct._2[0];
+    var paramsNew = MContext.getType(FQ_XXX).getOrError("Cannot get type " + FQ_XXX);
+    
+    var mapping = [Tup2.create(paramsCur, paramsNew)];
+
+    var actual = Utils.remap(t, mapping);
+    var expected = Context.typeof(Parse.parse("{ var x:hots.macros.utils.UtilsTest.V<" + FQ_XXX + "> = null; x;}"));
+    
+    Assert.isTrue(TypeExt.eq(actual, expected));
+  }
+  
+  public function testRemap2 () 
+  {
+    
+    var t = MContext.getType(FQ_V).getOrError("Cannot get type of " + FQ_V); // V<T>
+    
+    var ct = TypeExt.asClassType(t).getOrError("Cannot convert to Class Type"); // XXX
+    
+    var paramsCur = ct._2[0]; // T 
+    var paramsNew = MContext.getType(FQ_XXX).getOrError("Cannot get type " + FQ_XXX); // XXX
+    
+    var mapping = [Tup2.create(paramsCur, paramsNew)];
+
+    var actual = Utils.remap(t, mapping); // shoulb be V<XXX>
+    var expected = Parse.parseToType(FQ_V + "<" + FQ_XXX + ">").getOrError("Parse To Type Fails");
+    
+    Assert.isTrue(TypeExt.eq(actual, expected));
+  }
+
+  public function testGetParamsAsTypes () 
+  {
+    var paramsOption = MContext.getType(FQ_V).flatMap(
+      function (t) return TypeExt.asClassType(t).map(
+        function (x) return Utils.getParamsAsTypes(x._1.get())
+      )
+    );
+    
+    Assert.isTrue(paramsOption.isSome());
+  }
+  
+  public function testGetTypeParamMappings () {
+    
+    trace(MContext.getType("hots.macros.utils.UtilsTest.V"));
+    var t1 = 
+      TypeExt.asClassType(MContext.getType(FQ_V).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var t2 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_Z).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var param1 = Utils.getParamsAsTypes(t1)[0];
+    var expected = Some([Tup2.create(MContext.getType("StdTypes.Int").extract(), param1)]);
+    var actual = Utils.getTypeParamMappings(t2, t1);
+    
+    var mappingEq = function (a,b) return OptionExt.eq(a,b, 
+      function (a,b) return ArrayExt.eq(a,b, function (a,b) return Tup2Ext.eq(a,b, TypeExt.eq, TypeExt.eq)));
+    
+    Assert.isTrue(mappingEq(actual, expected));
+  }
+  
+  public function testGetTypeParamMappings2 () {
+    
+    trace(MContext.getType("hots.macros.utils.UtilsTest.V"));
+    var t1 = 
+      TypeExt.asClassType(MContext.getType(FQ_V).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var t2 = 
+      TypeExt.asClassType(MContext.getType(FQ_Z2).extract())
+      .map(function (x) return x._1.get()).extract();
+    
+    // expected K -> V.T
+    var t_K = t2.superClass.params[1];
+    var t_TT = t2.superClass.t.get().interfaces[0].params[0];
+    var t_T = Utils.getParamsAsTypes(t1)[0];
+    var expected = Some([Tup2.create(t_K, t_T)]);
+    var actual = Utils.getTypeParamMappings(t2, t1);
+    
+    var mappingEq = function (a,b) return OptionExt.eq(a,b, 
+      function (a,b) return ArrayExt.eq(a,b, function (a,b) return Tup2Ext.eq(a,b, TypeExt.eq, TypeExt.eq)));
+      
+    
+    Assert.isTrue(mappingEq(actual, expected));
+  }
+  
+  public function testGetFirstPath () {
+    var t1 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_U).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var t2 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_U).extract())
+      .map(function (x) return x._1.get()).extract();
+    
+    
+    var expected = Some([]);
+    var actual = Utils.getFirstPath(t1, t2);
+    
+    Assert.isTrue(OptionExt.eq(actual, expected, function (x,y) return ArrayExt.eq(x,y, PathNodeExt.eq))); 
+  }
+  
+  public function testGetFirstPath2 () {
+    var t1 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_U).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var t2 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_S).extract())
+      .map(function (x) return x._1.get()).extract();
+    
+    
+    var expected = Some([SuperClass]);
+    var actual = Utils.getFirstPath(t1, t2);
+    
+    Assert.isTrue(OptionExt.eq(actual, expected, function (x,y) return ArrayExt.eq(x,y, PathNodeExt.eq))); 
+  }
+  
+  public function testGetFirstPath3 () {
+    var t1 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_U).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var t2 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_C).extract())
+      .map(function (x) return x._1.get()).extract();
+    
+    
+    var expected = Some([SuperClass, InterfaceAt(0)]);
+    var actual = Utils.getFirstPath(t1, t2);
+    Assert.isTrue(OptionExt.eq(actual, expected, function (x,y) return ArrayExt.eq(x,y, PathNodeExt.eq))); 
+  }
+  
+  public function testGetFirstPath4 () {
+    var t1 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_U).extract())
+      .map(function (x) return x._1.get()).extract();
+      
+    var t2 = 
+      TypeExt.asClassType(Parse.parseToType(FQ_D).extract())
+      .map(function (x) return x._1.get()).extract();
+    
+    
+    // interfaces are sorted backwards C implements D, implements E, is stored in C.interfaces as [E,D], thus pathnode at 2 is InterfaceAt(1) 
+    var expected = Some([SuperClass, InterfaceAt(0), InterfaceAt(1)]);
+    var actual = Utils.getFirstPath(t1, t2);
+    trace(actual);
+    Assert.isTrue(OptionExt.eq(actual, expected, function (x,y) return ArrayExt.eq(x,y, PathNodeExt.eq))); 
+  }
   
   public function testIsCompatibleTo () {
     
