9e98b775442183159796cf28043cf5db0089030b
frabbit <frabbit77@googlemail.com>
2012-03-21 02:17:33 +0100

    cleaner flatMap functions for ArrayTOfMonad and OptionTOfMonad

--- src/hots/instances/ArrayTOfMonad.hx
+++ src/hots/instances/ArrayTOfMonad.hx
@@ -1,21 +1,14 @@
 package hots.instances;
-import hots.classes.Applicative;
 import hots.classes.MonadAbstract;
-import hots.classes.FunctorAbstract;
 import hots.classes.Monad;
 import hots.instances.ArrayTOfApplicative;
 
 import hots.In;
 import hots.Of;
 import scuts.core.extensions.ArrayExt;
-import hots.classes.Functor;
 
-import scuts.core.extensions.Function1Ext;
-import scuts.core.extensions.Function2Ext;
+using hots.macros.Box;
 
-private typedef B = hots.macros.Box;
-
-using hots.extensions.MonadExt;
 
 class ArrayTOfMonad<M> extends MonadAbstract<Of<M,Array<In>>> {
   
@@ -26,24 +19,20 @@ class ArrayTOfMonad<M> extends MonadAbstract<Of<M,Array<In>>> {
     super(ArrayTOfApplicative.get(monadM));
     this.monadM = monadM;
   }
-
+  
   override public function flatMap<A,B>(val:ArrayTOf<M,A>, f: A->ArrayTOf<M,B>):ArrayTOf<M,B> 
   {
-    var fmapped = monadM.flatMap(B.unbox(val), 
-      function (a) {
-        var mapped = ArrayOfFunctor.get().map(f, B.box(a));
-        var unboxed = B.unbox(mapped);
-        
-        var res = [];
-        for (e in unboxed) {
-          monadM.map(function (x:Array<B>) {
-            for (a in x) {
-              res.push(a);
-            }
-          },B.unbox(e));
-        }
-        return monadM.pure(res);//res;
-      });
-    return B.box(fmapped);
+    return monadM.flatMap(val.unbox(), function (a:Array<A>):Of<M, Array<B>>
+    {
+      var res = [];
+      for (e1 in a) 
+      {
+        monadM.map(
+          function (x:Array<B>) for (e2 in x) res.push(e2), 
+          f(e1).unbox()
+        );
+      }
+      return monadM.pure(res);
+    }).box();
   }
 }
--- src/hots/instances/OptionTOfMonad.hx
+++ src/hots/instances/OptionTOfMonad.hx
@@ -3,47 +3,35 @@ package hots.instances;
 import hots.In;
 import hots.Of;
 import scuts.core.types.Option;
-
-import scuts.core.extensions.OptionExt;
+using scuts.core.extensions.OptionExt;
 import hots.classes.Monad;
 import hots.classes.MonadAbstract;
 
-
 using scuts.core.extensions.Function1Ext;
 
-private typedef B = OptionBox;
-private typedef BT = OptionTBox;
+using hots.macros.Box;
 
-class OptionTOfMonadImpl<M> extends MonadAbstract<Of<M, Option<In>>> {
+class OptionTOfMonad<M> extends MonadAbstract<Of<M, Option<In>>> {
   
   var monadM:Monad<M>;
   
   public function new (monadM:Monad<M>) {
     super(OptionTOfApplicative.get(monadM));
     this.monadM = monadM;
+    
   }
   
-  override public function flatMap<A,B>(val:OptionTOf<M,A>, f: A->OptionTOf<M,B>):OptionTOf<M,B> 
+  override public function flatMap<A,B>(val:Of<Of<M, Option<In>>,A>, f: A->Of<Of<M, Option<In>>,B>):Of<Of<M, Option<In>>,B> 
   {
-    var fmapped = monadM.flatMap(BT.unbox(val), 
-      function (a) {
-        var mapped = OptionOfMonad.get().map(f, B.box(a));
-        var unboxed = B.unbox(mapped);
-
-        var res = switch (unboxed) {
-          case Some(v): 
-            var r = None;
-            monadM.map(function (x:Option<B>) r = x, BT.unbox(v));
-            r;
-            
-          case None: None;
-        }
-        return monadM.pure(res);//res;
-      });
-    return BT.box(fmapped);
+    return monadM.flatMap(val.unbox(), function (a)
+    {
+      return switch (a) 
+      {
+        case Some(v): f(v).unbox();
+        case None: monadM.pure(None);
+      }
+    }).box();
   }
   
 }
-
-typedef OptionTOfMonad = haxe.macro.MacroType<[hots.macros.TypeClasses.createProvider(OptionTOfMonadImpl)]>;
 
