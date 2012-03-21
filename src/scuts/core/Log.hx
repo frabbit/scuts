758738ab05b335d6fd3cb3b9098285b72c08ec42
frabbit <frabbit77@googlemail.com>
2012-03-21 02:22:30 +0100

    improved Logging (added compiler flag for log file creation for macro mode)

--- src/scuts/core/Log.hx
+++ src/scuts/core/Log.hx
@@ -1,21 +1,36 @@
 package scuts.core;
 import haxe.PosInfos;
-
+#if (macro || display)
+import neko.FileSystem;
+import neko.io.FileOutput;
+#end
 class Log 
 {
+  #if (macro || display)
+  static var logFile = "_scutsLog.txt";
+  static var logOutput:FileOutput;
+  static var logOutputCreated:Bool = false;
+  private static var writeLog:Bool = #if scutsLog true #else false #end;
+  #end
   public static function traced <T>(v:T, ?show:T->String, ?pos:PosInfos):T 
   {
-    #if debug
+    #if (debug)
     var t = if (show != null) show(v) else Std.string(v);
-    haxe.Log.trace(t, pos);
+    debugObj(v, t, pos);
     #end
     return v;
   }
   
   public static inline function debugObj <T,X> (e:T, msg:X, ?p:PosInfos) 
   {
-    #if (debug && scutsDebug)
+    #if (debug)
+    #if scutsDebug
     haxe.Log.trace(msg, p);
+    #elseif (macro && scutsLog)
+    if (writeLog && createLogFile()) {
+      logOutput.writeString(p.className + "." + p.methodName + "(" + p.lineNumber + "):" + msg + "\n");
+    }
+    #end
     #end
     return e;
   }
@@ -24,5 +39,24 @@ class Log
   {
     return debugObj(e, debug(e), p);
   }
+  
+  #if (macro || display)
+  static function createLogFile ():Bool {
+    return if (!logOutputCreated) {
+      logOutput = if (!FileSystem.exists(logFile)) 
+      {
+        neko.io.File.write(logFile, false);
+      } 
+      else 
+      {
+        neko.io.File.append(logFile, false);
+      }
+      true;
+    } else {
+      true;
+    }
+    
+  }
+  #end
   
 }
