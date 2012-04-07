fc84421ec9445c8c889db9927e10736f943d110a
frabbit <frabbit77@googlemail.com>
2012-04-07 03:48:37 +0200

    added first version of do syntax for monads example:
    var m = [1].box().tc(Monad);
    m.do_(
      x <= [2,3].box(),
      y <= [1,2].box(),
      x + y
    );

--- src/hots/extensions/MonadExt.hx
+++ src/hots/extensions/MonadExt.hx
@@ -1,9 +1,26 @@
 package hots.extensions;
+#if !macro
 import hots.instances.ArrayOfMonad;
 import hots.Of;
 import scuts.core.extensions.ArrayExt;
 import hots.classes.Monad;
 
+
+
+#end
+
+#if macro
+import scuts.core.types.Tup2;
+import scuts.mcore.Make;
+import scuts.mcore.Print;
+import scuts.mcore.Check;
+import haxe.macro.Expr;
+import scuts.mcore.Select;
+using scuts.mcore.extensions.ExprExt;
+using scuts.core.extensions.ArrayExt;
+using scuts.core.extensions.OptionExt;
+
+#end
 
 /**
  * ...
@@ -14,7 +31,38 @@ class MonadExt
 {
   
   
+  @:macro public static function do_<M>(m:ExprRequire<hots.classes.Monad<M>>,
+    e1:Expr, e2:Expr = null, e3:Expr = null, e4:Expr = null,
+    e5:Expr = null, e6:Expr = null, e7:Expr = null, e8:Expr = null,
+    e9:Expr = null, e10:Expr = null, e11:Expr = null
+    
+  )
+  {
+    var maybeNullExprs = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11];
+    var exprs = maybeNullExprs.filter(function (x) return !Check.isConstNull(x));
+    var last = exprs[exprs.length -1];
+    var head = exprs.removeLast();
+    
+    var res = head.foldRightWithIndex(
+      function (x, acc, index) {
+        var tup = Select.selectEBinopExprsWithOpFilter(x, function (op) return op == OpLte)
+        .map(function (t) return Tup2.create(Select.selectEConstCIdentValue(t._1).getOrError("Unsupported"), t._2))
+        .getOrElse(function () return Tup2.create("_", x));
+        
+        var f = Make.funcExpr([Make.funcArg(tup._1, false)], Make.returnExpr(acc));
+        var flatMapCall = Make.field(m, "flatMap").call([tup._2, f]);
+        
+        return flatMapCall;
+        
+      }, Make.field(m, "pure").call([last]));
+    
+    trace(Print.expr(res));
+    return res;
+  }
   
+  
+  #if !macro
+  /*
   public static function lift2 <M, A1, A2, R>(m:Monad<M>, f:A1->A2->R ):Of<M, A1>->Of<M, A2>->Of<M, R>
   {
     return function (v1:Of<M, A1>, v2:Of<M, A2>) {
@@ -55,7 +103,8 @@ class MonadExt
   {
     return sequence(mon, ArrayExt.map(a,f));
   }
-  
+  */
+  #end
   // from functor
   
   
