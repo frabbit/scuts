6a0a67ca34588fa831413d506bd3824bcd3cad6b
frabbit <frabbit77@googlemail.com>
2013-03-18 01:48:43 +0100

    move types to scutsCore

--- src/scuts/ds/ImList.hx
+++
@@ -1,9 +0,0 @@
-package scuts.core;
-
-/**
- * An immutable List.
- */
-enum ImList<T> {
-  Nil;
-  Cons(e:T, tail:ImList<T>);
-}
--- src/scuts/ds/LazyStack.hx
+++
@@ -1,7 +0,0 @@
-package scuts.ds;
-import scuts.ds.LazyList;
-using scuts.ds.LazyLists;
-
-@:native("scuts.ds.LazyList")
-extern class LazyStack<T> {}
-
