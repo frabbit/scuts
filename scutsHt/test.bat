472fe72704215c42febe4190345ac5ba9814e224
frabbit <frabbit77@googlemail.com>
2013-04-05 10:30:22 +0200

    more cleanup

--- scutsHt/test.bat
+++
@@ -1,3 +0,0 @@
-set HAXE_LIBRARY_PATH=..\..\..\sdksAndLibs\haxe-compile\bin\std
-..\..\..\sdksAndLibs\haxe-compile\bin\haxe.exe createImportAll.hxml
-REM ..\..\..\sdksAndLibs\haxe-compile\bin\haxe.exe runTests.hxml
