0a581032cfcf0c7d45ec76c4b7c8451c3572c14e
frabbit <frabbit77@googlemail.com>
2014-04-23 19:05:27 +0200

    only whitespace

--- scuts-ht/src/scuts/ht/macros/implicits/DisplayResolver.hx
+++ scuts-ht/src/scuts/ht/macros/implicits/DisplayResolver.hx
@@ -7,41 +7,42 @@ import haxe.macro.Expr;
 import haxe.macro.Context;
 
 
-class DisplayResolver 
+class DisplayResolver
 {
-  
-  public static function resolve (f:Expr, args:Array<Expr>, ?numArgs:Int = -1):Expr 
+  public static var helper(default, null) = macro scuts.ht.macros.syntax.DoHelper;
+
+  public static function resolve (f:Expr, args:Array<Expr>, ?numArgs:Int = -1):Expr
   {
    if (numArgs == null) numArgs == -1;
-    
-    
+
+
     var numArgsNew:Int = if (numArgs != -1) {
       numArgs;
     } else {
       var t = try Context.typeof(f) catch (e:Dynamic) null;
-      
+
       if (t != null) {
         switch (t) {
           case TFun(fargs, _):
             var x : Int = fargs.length; x;
-          case _ : var x : Int = -1; x; 
-          
+          case _ : var x : Int = -1; x;
+
         }
       } else { var x : Int = -1; x; }
     }
-  
-    
-    var numArgsNew = -1;
-    
 
-    
+
+    var numArgsNew = -1;
+
+
+
     var t1:Expr = if (numArgsNew != -1) {
-      
+
       var len = numArgsNew - args.length;
       var hasWildCards = false;
       for (a in args) {
         switch (a.expr) {
-          case EConst(CIdent("_")): 
+          case EConst(CIdent("_")):
             hasWildCards = true;
             break;
           case _:
@@ -51,20 +52,20 @@ class DisplayResolver
         args.push(macro null);
       }
       var f1:Expr = if (hasWildCards) macro $f.bind else f;
-      
+
       var x : Expr = macro $f1($a{args});
       x;
-      
+
     } else {
-      
-      var x : Expr = macro $f($a{args});  
+
+      var x : Expr = macro $f($a{args});
       x;
     }
     return t1;
-    
+
   }
-  
-  public static function resolveImplicitObjByType (t:String):Expr 
+
+  public static function resolveImplicitObjByType (t:String):Expr
   {
     return Context.parse(" { var e : " + t + " = null; e; } ", Context.currentPos());
   }
--- scuts-ht/src/scuts/ht/macros/syntax/DoTools.hx
+++ scuts-ht/src/scuts/ht/macros/syntax/DoTools.hx
@@ -2,11 +2,11 @@ package scuts.ht.macros.syntax;
 
 #if macro
 
-#if display
+#if (display)
 
 typedef DoTools = scuts.ht.macros.syntax.DisplayDoTools;
 
-#else 
+#else
 
 typedef DoTools = scuts.ht.macros.syntax.RealDoTools;
 
