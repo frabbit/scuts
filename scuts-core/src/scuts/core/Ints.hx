237aaf8dea6a8ef5afd014f9e62baaa54c889de2
frabbit <frabbit77@googlemail.com>
2014-07-08 15:46:53 +0200

    add compare

--- scuts-core/src/scuts/core/Ints.hx
+++ scuts-core/src/scuts/core/Ints.hx
@@ -1,30 +1,32 @@
 package scuts.core;
 
-class Ints 
+class Ints
 {
 
-  
+
   public static inline  function plus(a:Int, b:Int):Int return a + b;
   public static inline  function minus(a:Int, b:Int):Int return a - b;
   public static inline  function multiply(a:Int, b:Int):Int return a * b;
-  
+
   public static inline  function divide(a:Int, b:Int):Float return a / b;
 
   public static inline  function div(a:Int, b:Int):Int return Std.int(a / b);
 
   public static inline  function eq(a:Int, b:Int):Bool return a == b;
-  
+
   public static inline function max(a:Int, b:Int) return a > b ? a : b;
-  
+
   public static inline function min(a:Int, b:Int) return a < b ? a : b;
 
   @:noUsing public static inline function ceil(x:Float):Int return Std.int(Math.ceil(x));
-  
+
   public static inline function inRange(i:Int, min:Int, maxExcluded:Int) return i >= min && i < maxExcluded;
 
   public static inline function toBool(i:Int) return i != 0;
-  
-  
-  
-  
+
+  public static inline function compare(a:Int, b:Int):Int return a < b ? -1 : (a > b) ? 1 : 0;
+
+
+
+
 }
