78092744859f78b7dba915c97c27f9b65aa03cea
frabbit <frabbit77@googlemail.com>
2015-02-09 19:32:59 +0100

    add toArray

--- scuts-core/src/scuts/core/Lists.hx
+++ scuts-core/src/scuts/core/Lists.hx
@@ -2,20 +2,30 @@ package scuts.core;
 
 /**
  * ...
- * @author 
+ * @author
  */
 
-class Lists 
+class Lists
 {
 
-  public static inline function map < A, B > (list:List<A>, f:A->B):List<B> 
+  public static inline function map < A, B > (list:List<A>, f:A->B):List<B>
   {
     return Iterables.mapToList(list, f);
   }
-  
-  public static inline function mapWithIndex < A, B > (list:List<A>, f:A->Int->B):List<B> 
+
+  public static inline function mapWithIndex < A, B > (list:List<A>, f:A->Int->B):List<B>
   {
     return Iterables.mapToListWithIndex(list, f);
   }
-  
+
+  public static inline function mapWithIndex < A, B > (list:List<A>, f:A->Int->B):List<B>
+  {
+    return Iterables.mapToListWithIndex(list, f);
+  }
+
+  public static inline function toArray < A > (list:List<A>):Array<A>
+  {
+    return [for (e in list) e];
+  }
+
 }
