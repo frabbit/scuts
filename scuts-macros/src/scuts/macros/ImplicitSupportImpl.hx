95c702dc835bc3d8e47f4ab6a784fdf07aa8ce4f
frabbit <frabbit77@googlemail.com>
2015-03-06 16:42:03 +0100

    remove traces, causes problems here

--- scuts-macros/src/scuts/macros/ImplicitSupportImpl.hx
+++ scuts-macros/src/scuts/macros/ImplicitSupportImpl.hx
@@ -11,17 +11,6 @@ class ImplicitSupportImpl {
 	{
 		var allFields = Context.getBuildFields();
 
-		try {
-			trace(Context.typeof(macro scuts.ht.core.Ht.implicit));	
-		} catch (e:haxe.macro.Expr.Error) {
-			
-			if (e.message == "Macro functions must be called immediately") {
-				trace("is Macro");
-			}
-			
-		}
-		
-
 		allFields.map(convertField);
 
 		return allFields;
@@ -30,15 +19,15 @@ class ImplicitSupportImpl {
 	}
 
 	static function convertField (x:Field) {
-		
+
 		return switch (x.kind) {
 			case FieldType.FFun(f):
 				f.expr = convertExpr(f.expr);
-				trace(ExprTools.toString(f.expr));
+				//trace(ExprTools.toString(f.expr));
 				x;
 			case _: x;
 		}
-		
+
 	}
 
 	static function convertExpr (e:Expr) {
@@ -46,48 +35,48 @@ class ImplicitSupportImpl {
 		return switch (e.expr) {
 			case EConst(CIdent(s)) if (s != "$type" && s.charAt(0) == "$" && s.length > 1):
 				var e = macro $i{s.substr(1)};
-				macro scuts.ht.core.Ht.implicit($e);	
+				macro scuts.ht.core.Ht.implicit($e);
 
-			
+
 
 			case ECall({expr:EField(e1, n)}, params) if (n == "_"):
-				
+
 				var args = [convertExpr(e1)].concat(params.map(convertExpr));
-				macro scuts.ht.core.Ht.resolve($a{args});	
-			
+				macro scuts.ht.core.Ht.resolve($a{args});
+
 			case ECall(e1 = {expr:EField(e2, "bind")}, params):
-				
+
 				var newParams = params.filter(function (e) return switch (e.expr) {
 					case EConst(CIdent("__")): false;
 					case _ : true;
 				});
 				if (newParams.length != params.length) {
 					var args = [convertExpr(e2)].concat(newParams.map(convertExpr));
-					macro scuts.ht.core.Ht.resolve($a{args});	
+					macro scuts.ht.core.Ht.resolve($a{args});
 				} else {
-					ExprTools.map(e, convertExpr);	
+					ExprTools.map(e, convertExpr);
 				}
-			case ECall(e1, params): 
+			case ECall(e1, params):
 				var newParams = params.filter(function (e) return switch (e.expr) {
 					case EConst(CIdent("_")): false;
 					case _ : true;
 				});
 				if (newParams.length != params.length) {
 					var args = [convertExpr(e1)].concat(newParams.map(convertExpr));
-					macro scuts.ht.core.Ht.resolve($a{args});	
+					macro scuts.ht.core.Ht.resolve($a{args});
 				} else {
-					ExprTools.map(e, convertExpr);	
+					ExprTools.map(e, convertExpr);
 				}
-				
+
 			case _:
 				ExprTools.map(e, convertExpr);
 		}
-		
 
 
-		
+
+
 
 	}
-	
+
 
 }
